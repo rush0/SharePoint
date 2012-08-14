52947ea3e34e44c520d00b30860c6c82281e700a
Omar <rush0@rush0g.com>
2012-08-13 21:31:48 -0400

    View Field Visual Sorting Done
    Updates UI, doesn't yet update select field values.

---
+++ Update View Field Order/SortViewOrder.js
@@ -0,0 +1,23 @@
+﻿$('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody').sortable({
+
+    items: 'tr:not(:first-child)',
+
+    helper: function (e, ui) {
+        ui.children().each(function () {
+            $(this).width($(this).width());
+        });
+        return ui;
+    },
+
+    update: function () {
+        $('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody tr:not(:first-child) select').each(function (indx) {
+            var select = $(this);
+
+            var selectedVal = select.children('option').filter(function () {
+                return $(this).text() == indx + 1;
+            }).val();
+
+            select.val(selectedVal);
+        });
+    }
+});
