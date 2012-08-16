03fcef0ed2c82e6e57cef9881e4917985f6748cf
Omar Stewey <rush0g@gmail.com>
2012-08-15 21:13:16 -0400

    Updated to auto-select checkbox, Hide column order

--- Update View Field Order/SortViewOrder.js
+++ Update View Field Order/SortViewOrder.js
@@ -1,25 +1,48 @@
-﻿$('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody tr:not(:first-child) select').removeAttr("onchange");
+﻿$(function () {
 
-$('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody').sortable({
+    var orderTable = $('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody');
+    var orderSelects = $('tr:not(:first-child) select', orderTable);
 
-    items: 'tr:not(:first-child)',
+    orderSelects.removeAttr("onchange").hide();
 
-    helper: function (e, ui) {
-        ui.children().each(function () {
-            $(this).width($(this).width());
-        });
-        return ui;
-    },
+    $('b', orderTable).filter(function () {
+        return $(this).text() == 'Position from Left';
+    }).text('');
 
-    update: function () {
-        $('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody tr:not(:first-child) select').each(function (indx) {
-            var select = $(this);
 
-            var selectedVal = select.children('option').filter(function () {
-                return $(this).text() == indx + 1;
-            }).val();
+    var sortConfig = {
 
-            select.val(selectedVal);
-        });
-    }
+        items: 'tr:not(:first-child)',
+
+        helper: function (e, ui) {
+            // fix width glitch with table rows: foliotek.com/devblog/make-table-rows-sortable-using-jquery-ui-sortable/
+            ui.children().each(function () {
+                $(this).width($(this).width());
+            });
+            return ui;
+        },
+        start: function (e, ui) {
+            ui.item.find('input').attr('checked', 'checked');
+        },
+
+        update: function () {
+            orderSelects = $('tr:not(:first-child) select', orderTable);
+            orderSelects.each(function (indx) {
+                var select = $(this);
+
+                var selectedVal = select.children('option').filter(function () {
+                    return $(this).text() == indx + 1;
+                }).val();
+
+                select.val(selectedVal);
+            });
+        },
+
+
+    };
+
+    orderTable.sortable(sortConfig);
+
 });
+
+
