d26e7e1155f185f5df8b14adb70bc1178d82c47d
Omar Stewey @ Work <rush0g@gmail.com>
2012-08-16 13:00:58 -0400

    v1
    Sandboxed Solution

---
+++ Update View Field Order/OSLabsViewFieldSorting/Assets/viewsettings.debug.js
@@ -0,0 +1,54 @@
+﻿jQuery.noConflict()(function () {
+
+    if(location.href.match(/viewedit.aspx/gi)){
+
+    var siteUrl = _spPageContextInfo.siteServerRelativeUrl;
+    var css = siteUrl + "/OSLabs/CSS/ViewFieldSorting.css";
+
+    jQuery('head').append('<link rel="stylesheet" href="'+ css +'" type="text/css" /> ');
+
+    var orderTable = jQuery('tbody#tbodyViewColumns > tr > td.ms-authoringControls tbody');
+    var orderSelects = jQuery('tr:not(:first-child) select', orderTable);
+
+    orderSelects.removeAttr("onchange").hide();
+
+    jQuery('b', orderTable).filter(function () {
+        return jQuery(this).text() == 'Position from Left';
+    }).text('');
+
+
+    var sortConfig = {
+
+        items: 'tr:not(:first-child)',
+
+        helper: function (e, ui) {
+            // fix width glitch with table rows: foliotek.com/devblog/make-table-rows-sortable-using-jquery-ui-sortable/
+            ui.children().each(function () {
+                jQuery(this).width(jQuery(this).width());
+            });
+            return ui;
+        },
+        start: function (e, ui) {
+            ui.item.find('input').attr('checked', 'checked');
+        },
+
+        update: function () {
+            orderSelects = jQuery('tr:not(:first-child) select', orderTable);
+            orderSelects.each(function (indx) {
+                var select = jQuery(this);
+
+                var selectedVal = select.children('option').filter(function () {
+                    return jQuery(this).text() == indx + 1;
+                }).val();
+
+                select.val(selectedVal);
+            });
+        },
+
+    };
+
+    orderTable.sortable(sortConfig);
+    }
+});
+
+
---
+++ Update View Field Order/OSLabsViewFieldSorting/Features/ViewFieldSorting/ViewFieldSorting.feature
@@ -0,0 +1,8 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d4f69eeb-3f74-4440-a422-b0fa2f49cda6" description="Allows for enhanced sorting on View Setting pages. Field ordering can be reordered by dragging and dropping in desired order." featureId="d4f69eeb-3f74-4440-a422-b0fa2f49cda6" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Enahcned View Field Sorting" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c23846df-8b7c-4469-bef3-c8a9c142efb8" />
+    <projectItemReference itemId="d564c499-87a6-4ffe-9912-32afabd217c7" />
+    <projectItemReference itemId="103846c0-d721-436d-abfc-3633508fe6da" />
+  </projectItems>
+</feature>
